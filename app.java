a6e2dd2efa11fc5b4a3960008da8040d0b7cc395
AsharRehman450 <asharrehmanali@gmail.com>
2024-09-09 00:43:19 +0500

    add files and folder

---
+++ app.java
@@ -0,0 +1,303 @@
+// chapter1 alert
+
+// alert("error! please enter a valid password");
+
+// Q3
+
+// alert("welcome to js land..\nhappy coding!");
+
+// Q4
+// alert("welcome to js land");
+
+// alert("happy coding");
+
+// Q5
+
+
+
+// hello i can erun js through web browser console
+
+// Q6
+
+// alert("hello !");
+
+// Q7
+
+// b is correct answer
+
+
+// chapter 2
+
+// Q1
+
+// var  username;
+
+// Q2
+
+// var myname = "ashar rehman ali";
+
+// console.log(myname);
+
+// Q3
+
+// var message;
+
+// message = "hello world";
+
+// alert(message);
+
+// Q4
+
+// var name = "john doe";
+
+// var age = 15;
+
+// var course = "certified mob web dev";
+
+
+// alert (name);
+
+// alert (age);
+
+// alert (course);
+
+// Q5
+
+
+// alert("Pizza\nPizz\nPiz\nPi\nP");
+
+// Q6
+
+// var email;
+
+// email = "ashar@abc.com";
+
+// alert("my email address is" + " " + email);
+
+
+// Q7
+
+// var book;
+
+// book = "a smarter way to learn java script"
+
+// alert(" i am trying to learn a book" + " " + book);
+
+
+// Q8
+
+// var word = "yah! i can write html content through javascript"
+
+// document.write(word);
+
+// Q9
+
+// alert("▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+// chapter 3
+
+// Q1
+// var age;
+
+// age = 18;
+
+// alert(age);
+
+
+// Q2 *
+
+// var number = 14;
+
+// alert("you have visited this site" + number + " times");
+
+// Q3 *
+
+// var birthyear =2005;
+
+// document.write("My birth year is " + birthyear + "<br> Data type of my declared varibale is number");
+
+// Q4 
+
+// var visitor = "John Doe";
+
+// var product = " ordered 5 T-shirt(s)";
+
+// var quantity = " XYZ clothing store";
+
+// document.write(visitor,product,quantity);
+
+// chapter 4
+
+//  1.
+
+//  var age,birthyear,height;
+
+// console.log();
+
+// 2.
+
+//  5 legal varibales
+
+// var $myVar;
+
+// var _mystring;
+
+// var name123;
+
+// var monday456;
+
+// var input;
+
+//  5 illegal varibales
+
+// var 12;
+
+// var 3224 num;
+
+// var 2435 word;
+
+// var num$;
+
+// var 12monday;
+
+// 3.
+
+// var heading = "<h2> Rules For Naming JS Variables <h2>";
+
+// document.write(heading);
+
+// 4.
+
+// var number = "numbers";
+
+// var dollar = " $";
+
+// var variable = "_";
+
+// document.write("Varibale name can only contain " + number,dollar + " and" + variable + ". For example : my _1stVariable");
+
+// 5.
+
+// var letter = "letters";
+
+// var dollar = " $";
+
+// var variable = "_";
+
+// document.write("Varibale name can only contain " + letter,dollar + " or" + variable + ". For example : $name, _name or name");
+
+// 5.
+
+// var word = "keywords"
+
+// document.write("Variable should not be JS" + " " word);
+
+// 6.
+
+// var name = "sensitive"
+
+// document.write("Variable names are case" + " " + name);
+
+//    ASSINGMENT 
+
+   // * chapter 1  *
+
+// 1.
+
+//   alert("Error please enter a valid name");
+
+//    alert("Error please enter a valid fullname");
+
+//   alert("Error please enter a valid email");
+
+//   alert("Error please enter a valid phone number");
+
+//   alert("Error please enter a valid password");
+
+// 2. 
+
+// alert("you're learning in javascript");
+
+// 3.
+
+// var qoute;
+
+// qoute = "Its's just the beginning many more to come";
+
+// alert(qoute);
+
+// CHAPTER 2
+
+// 1.
+
+// var firstName;
+
+// 2.
+
+// var myVariable;
+
+// myVariable = "Hello World!"
+
+// 3.
+
+// var teamName = "The Avengers";
+
+// alert(teamName);
+
+// 4.
+
+// var bestMan = "charlie"
+
+// bestMan = "JohnDoe";
+
+//  CHAPTER 3
+
+// 1.
+
+// var caseQty;
+
+
+// 2.
+
+// caseQty = 144;
+
+// 3.
+
+// var num = 9;
+
+// 4.
+
+// var result;
+
+// result = 5 + 7;
+
+// 5.
+
+// var merchTotal = 100;
+
+// var shippingCharge = 10;
+
+// console.log(merchTotal + shippingCharge );
+
+// 6.
+
+// var originalnum = 3;
+
+// var newnum = originalnum + 7;
+
+// console.log(originalnum + newnum);
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
